f6c2e382b653f7d464f29a16fc55f39b358d4170
David Quinn <david@urbmet.com>
2012-12-07 17:03:31 -0500

    refined walkthrough. results appear to make sense, and generated appropriate shapes; node issue still unresolved, but using street geometries which suffice

--- README.md
+++ README.md
@@ -3,24 +3,22 @@ ncg_cityscale
 
 This describes the workflow for visualization of dublin traffic patterns. There are three pieces associated with this:
 
- - 1: Routing calculations: user supplie start-end address with desired arrival time. The user can then also choose a time range which enables them to observe diferent routes. Summary visualization of different routes shown on screen that user can cycle through
+ - 1: Routing calculations: user supply start-end address with desired arrival time. The user can then also choose a time range which enables them to observe diferent routes. Summary visualization of different routes shown on screen that user can cycle through
  
  - 2: Area performance: user can use a pre-defined square or draw a polygon to assess the traffic flows in an area
 
-1: Routing Patterns
--------------------
+### 1: Routing Patterns
 
 
 
-2: Area Performance
--------------------
+### 2: Area Performance
 
 
 
-3: Iso-Calculations
--------------------
+### 3: Iso-Calculations
+
 Added an image of the iso-calculation here to demonstrate what it looks like:
-[isochrone] [1]. This used `PgRouting` (and much advice from @underdark [here][2]). 
+[isochrone] [1]. This used `PgRouting` (and much advice from @anitagraser [here][2]). 
 
 [1]: http://www.flickr.com/photos/90859215@N03/8252169937/
 [2]: http://underdark.wordpress.com/2011/09/25/a-closer-look-at-alpha-shapes-in-pgrouting/
--- sql/alpha_walkthrough.sql
+++ sql/alpha_walkthrough.sql
@@ -17,8 +17,8 @@ CREATE INDEX idx_spatial_dublin_nodes ON dublin_nodes USING gist (geom);
 
 -- now start to construct reach using great instructions from here:
 -- http://underdark.wordpress.com/2011/09/25/a-closer-look-at-alpha-shapes-in-pgrouting/
--- first calculate cost within 10 km distance from random node "23376"
--- a 10 km catchment zone around node #23376 in osm road network
+-- first calculate cost within 10 km distance from random node "4052"
+-- a 10 km catchment zone around node #4052 in osm road network
 -- join the results of driving_distance() with the table containing node geometries
 DROP TABLE tmp_cost;
 CREATE TABLE tmp_cost AS
@@ -31,12 +31,12 @@ SELECT *
           target::int4 AS target,
           length::float8 AS cost
       FROM dublin_traffic',
-      23376,
+      4052,
       10000,
       false,
       false)) AS route
    ON
-   dublin_nodes.id = route.vertex_id
+   dublin_nodes.id = route.vertex_id;
 
    -- there is something strange going on here
    -- can't tell if it is the points joining on the wrong ID, or something else
@@ -53,36 +53,72 @@ SELECT *
               target::int4 AS target,
               length::float8 AS cost
           FROM dublin_traffic',
-          23376,
+          4052,
           10000,
           false,
           false)) AS route
        ON
-       d.target = route.vertex_id
+       d.target = route.vertex_id;
        -- this works fine (must be a problem with nodes/node ID)
 
 
 -- The following queries create the table and insert an alpha shape for all points with a cost of less than 1500:
 -- this uses a ConvexHull (is concave more appropriate?)
 DROP TABLE home_isodist;
-CREATE TABLE home_isodist (id serial, max_cost double precision);
+CREATE TABLE home_isodist (gid serial, max_cost double precision);
 SELECT AddGeometryColumn('home_isodist','geom',900913,'POLYGON',2);
 
 -- convex
+/*
 INSERT INTO home_isodist (max_cost, geom) (
   SELECT 
     1500, 
     ST_ConvexHull(ST_Collect(geom)) As geom
   FROM tmp_cost 
   WHERE cost < 1500);
+*/
 
 -- concave
 INSERT INTO home_isodist (max_cost, geom) (
   SELECT 
+    500, 
+    ST_ConcaveHull(ST_Collect(geom), 0.75) As geom
+  FROM tmp_cost 
+  WHERE cost < 500);
+
+INSERT INTO home_isodist (max_cost, geom) (
+  SELECT 
+    1000, 
+    ST_ConcaveHull(ST_Collect(geom), 0.75) As geom
+  FROM tmp_cost 
+  WHERE cost < 1000);
+
+  INSERT INTO home_isodist (max_cost, geom) (
+  SELECT 
     1500, 
-    ST_ConcaveHull(ST_Collect(geom), 0.99) As geom
+    ST_ConcaveHull(ST_Collect(geom), 0.75) As geom
   FROM tmp_cost 
   WHERE cost < 1500);
+
+  INSERT INTO home_isodist (max_cost, geom) (
+  SELECT 
+    2000, 
+    ST_ConcaveHull(ST_Collect(geom), 0.75) As geom
+  FROM tmp_cost 
+  WHERE cost < 2000);
+
+-- currently loading using mapserver
+
+
+-- to produce a PNG directly (instead of via mapserver)
+-- not sure if I can align boundaries with map image
+SELECT encode(
+              ST_AsPNG(
+                        ST_AsRaster(geom, 400, 400, '2BUI')
+                        ), 
+              'base64')
+FROM home_isodist
+WHERE max_cost = 1500;
 
 -- need to make this work with model simulation output
 /*   
