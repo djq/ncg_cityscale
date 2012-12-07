769fb3cc42af15d26607e69ad05a6ba9b89fed77
David Quinn <david@urbmet.com>
2012-12-07 11:10:58 -0500

    expanded walk-through example

--- sql/alpha_walkthrough.sql
+++ sql/alpha_walkthrough.sql
@@ -11,10 +11,28 @@ SELECT * FROM driving_distance('
       false,
       false)
 
+-- now create a table of nodes 
+DROP TABLE dublin_nodes;
+CREATE TABLE dublin_nodes AS
+SELECT
+  id,
+  ST_MakePoint(y1, x1, 900913) as geom
+FROM dublin_traffic;
+
+CREATE UNIQUE INDEX idx_dublin_nodes ON dublin_nodes (id);
+CREATE INDEX idx_spatial_dublin_nodes ON dublin_nodes USING gist (geom);
+
+-- update dublin_traffic with length of geometries
+-- add a new column
+ALTER TABLE dublin_traffic ADD length numeric(10,3);
+-- update with length
+UPDATE dublin_traffic 
+SET length = ST_Length(geom);
+
 -- now start to construct reach
 -- join the results of driving_distance() with the table containing node geometries:
 SELECT *
-   FROM dublin_traffic
+   FROM dublin_nodes
    JOIN
    (SELECT * FROM driving_distance('
       SELECT gid AS id,
@@ -23,30 +41,32 @@ SELECT *
           cost::float8 AS cost
       FROM dublin_traffic',
       42620,
-      10000,
+      1000,
       false,
       false)) AS route
    ON
-   dublin_traffic.id = route.vertex_id
+   dublin_nodes.id = route.vertex_id
 
 -- http://underdark.wordpress.com/2011/09/25/a-closer-look-at-alpha-shapes-in-pgrouting/
 -- a 10 km catchment zone around node #2699 in my osm road network
-CREATE TABLE home_catchment10km AS
-SELECT *
-   FROM osm_nodes
+DROP TABLE tmp_cost;
+CREATE TABLE tmp_cost AS
+SELECT 
+  *  
+   FROM network_nodes as d
    JOIN
    (SELECT * FROM driving_distance('
       SELECT gid AS id,
-          source,
-          target,
-          meters AS cost
-      FROM osm_roads',
-      2699,
-      10000,
+          fromid::int4 as source,
+          toid::int4 AS target,
+          length::float8 AS cost
+      FROM network_links',
+      319029606,
+      1000,
       false,
-      false)) AS route
+      false)) AS r
    ON
-   osm_nodes.id = route.vertex_id
+   d.id::int4 = r.vertex_id
 
 
 
