78f9016e2b860cab8b3224a77b9b3eba5bb8b8ce
David Quinn <david@urbmet.com>
2012-12-10 18:54:40 -0500

    tested function using a column of values as input. Nest SQL clause need to retrieve results

--- sql/_calculate_distances.sql
+++ sql/_calculate_distances.sql
@@ -1,54 +1,36 @@
 -- Function to calculate driving distances for a column of IDs
 -- this uses the working code in file 'alpha_walkthrough'
-
-/*
-DROP TABLE tmp_cost;
-CREATE TABLE tmp_cost AS
-SELECT 
-   d.geom,
-   route.cost
-   FROM dublin_traffic as d
-   JOIN
-   (SELECT * FROM driving_distance('
-      SELECT gid AS id,
-          source::int4 AS source,
-          target::int4 AS target,
-          length::float8 AS cost
-      FROM dublin_traffic',
-      4052,
-      10000,
-      false,
-      false)) AS route
-   ON
-   d.target = route.vertex_id;
-*/
-
--- this functions assumes the existence of two other tables
--- 1] dublin_traffic
--- 2] column of road_id values that can be passed in
+-- this functions assumes the existence of the 'dublin_traffic' table
 
 -- drop table and function for _ncg_driving_distance
-DROP FUNCTION _ncg_driving_distance( integer );
+DROP FUNCTION _ncg_driving_distance( integer, double precision );
 DROP TYPE _ncg_driving_distance_table;
 
+/***************
+table definition
+***************/
 -- create object type for _ncg_driving_distance
 CREATE TYPE _ncg_driving_distance_table AS (
 	road_id integer, 
-	geom    geometry, 
-	cost    double precision
-	);
+	cost    double precision,
+	geom    geometry );
 
+/******************
+function definition
+*******************/
 CREATE OR REPLACE FUNCTION _ncg_driving_distance(
-	road_id    integer
+	road_id    integer,
+	distance   double precision
 	)
 RETURNS SETOF _ncg_driving_distance_table AS $$
 BEGIN  
   RETURN QUERY 
   SELECT
-   road_id,
-   d.geom,
-   route.cost
-   FROM dublin_traffic as d
+   	road_id,
+   	route.cost,
+   	d.geom   
+  FROM 
+  	dublin_traffic as d
    JOIN
    (SELECT * FROM driving_distance('
       SELECT gid AS id,
@@ -56,8 +38,8 @@ BEGIN
           target::int4 AS target,
           length::float8 AS cost
       FROM dublin_traffic',
-      4052,
-      10000,
+      road_id,
+      distance,
       false,
       false)) AS route
    ON
@@ -65,3 +47,21 @@ BEGIN
   END;
 $$ LANGUAGE plpgsql;
 
+/************
+testing query
+*************/
+-- test code for one ID
+/*
+SELECT * FROM _ncg_driving_distance(4052, 1000.0);
+*/
+
+-- testing using a column is more complicated
+/*
+DROP TABLE tmp_cost;
+CREATE TABLE test_function_dist AS
+SELECT (t2.function_row).road_id,
+       (t2.function_row).cost,
+       (t2.function_row).geom,
+FROM (SELECT _ncg_driving_distance(t.road_id, 1000.0) as function_row
+     FROM _fishnet_road_source t ) t2;
+*/
